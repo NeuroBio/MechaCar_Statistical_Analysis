4b5a99bb3128138d3cd4654ef344ae053ac26562
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-15 09:40:31 -0400

    complete code for deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,8 +3,25 @@
 # the above packages rely on rtools40
 
 library(dplyr)
+
+# Deliverable 1:
 mechaCarData <- read.csv('MechaCar_mpg.csv')
 
 mpgModel <- lm(mpg ~ vehicle_length + vehicle_weight +
                  spoiler_angle + ground_clearance + AWD, mechaCarData)
+
 summary(mpgModel)
+
+
+# Deliverable 2
+coilData <- read.csv('Suspension_Coil.csv')
+
+total_summary <- summarize(coilData, Mean = mean(PSI),
+                           Median = median(PSI),
+                           Variance = var(PSI),
+                           SD = sd(PSI))
+lot_summary <- coilData %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI),
+            Median = median(PSI),
+            Variance = var(PSI),
+            SD = sd(PSI), .groups = 'keep')
